43f776a59ab88cf8968d06d27489f15adcf0b255
German Alvarez <kenchic@gmail.com>
2015-05-19 17:19:12 -0500

    Fuentes
    Inicio del Proyecto

---
+++ Fuentes/AutoGenerador/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AutoGenerador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.buttonEdit1 = new DevExpress.XtraEditors.ButtonEdit();
+            this.checkEdit1 = new DevExpress.XtraEditors.CheckEdit();
+            this.checkEdit2 = new DevExpress.XtraEditors.CheckEdit();
+            this.checkEdit3 = new DevExpress.XtraEditors.CheckEdit();
+            this.checkEdit4 = new DevExpress.XtraEditors.CheckEdit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit3.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit4.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(12, 30);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(27, 13);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "Ruta:";
+            // 
+            // buttonEdit1
+            // 
+            this.buttonEdit1.Location = new System.Drawing.Point(58, 27);
+            this.buttonEdit1.Name = "buttonEdit1";
+            this.buttonEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.buttonEdit1.Size = new System.Drawing.Size(404, 20);
+            this.buttonEdit1.TabIndex = 2;
+            this.buttonEdit1.ButtonClick += new DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(this.buttonEdit1_ButtonClick);
+            // 
+            // checkEdit1
+            // 
+            this.checkEdit1.Location = new System.Drawing.Point(12, 67);
+            this.checkEdit1.Name = "checkEdit1";
+            this.checkEdit1.Properties.Caption = "Entidades";
+            this.checkEdit1.Size = new System.Drawing.Size(75, 19);
+            this.checkEdit1.TabIndex = 3;
+            // 
+            // checkEdit2
+            // 
+            this.checkEdit2.Location = new System.Drawing.Point(12, 92);
+            this.checkEdit2.Name = "checkEdit2";
+            this.checkEdit2.Properties.Caption = "DALC";
+            this.checkEdit2.Size = new System.Drawing.Size(75, 19);
+            this.checkEdit2.TabIndex = 4;
+            // 
+            // checkEdit3
+            // 
+            this.checkEdit3.Location = new System.Drawing.Point(12, 117);
+            this.checkEdit3.Name = "checkEdit3";
+            this.checkEdit3.Properties.Caption = "Fachada";
+            this.checkEdit3.Size = new System.Drawing.Size(75, 19);
+            this.checkEdit3.TabIndex = 5;
+            // 
+            // checkEdit4
+            // 
+            this.checkEdit4.Location = new System.Drawing.Point(12, 143);
+            this.checkEdit4.Name = "checkEdit4";
+            this.checkEdit4.Properties.Caption = "Fabrica";
+            this.checkEdit4.Size = new System.Drawing.Size(75, 19);
+            this.checkEdit4.TabIndex = 6;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(376, 159);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 7;
+            this.simpleButton1.Text = "Generar";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 185);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.checkEdit4);
+            this.Controls.Add(this.checkEdit3);
+            this.Controls.Add(this.checkEdit2);
+            this.Controls.Add(this.checkEdit1);
+            this.Controls.Add(this.buttonEdit1);
+            this.Controls.Add(this.labelControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit3.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit4.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.ButtonEdit buttonEdit1;
+        private DevExpress.XtraEditors.CheckEdit checkEdit1;
+        private DevExpress.XtraEditors.CheckEdit checkEdit2;
+        private DevExpress.XtraEditors.CheckEdit checkEdit3;
+        private DevExpress.XtraEditors.CheckEdit checkEdit4;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+
+    }
+}
+
---
+++ Fuentes/AutoGenerador/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoGenerador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEdit1_ButtonClick(object sender, DevExpress.XtraEditors.Controls.ButtonPressedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+                buttonEdit1.Text = fbd.SelectedPath + "\\";
+
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            StringBuilder archivo = new StringBuilder();
+            string propiedades = "";
+            string metodos = "";
+
+            if (checkEdit1.Checked)
+            {
+                DataTable datTablas = cargarEsquemaBD();
+                if (datTablas != null)
+                {
+                    foreach (DataRow dr in datTablas.Rows)
+                    {
+                        System.IO.StreamReader reader = new System.IO.StreamReader("estructuras\\Entidad.txt");
+                        archivo = new StringBuilder(reader.ReadToEnd());
+                        reader.Close();
+
+                        string NombreTabla = dr[2].ToString();
+                        DataTable datCampos = cargarEsquemaTabla(NombreTabla);
+
+                        archivo.Replace("[TABLE]", "cls" + NombreTabla.Substring(2));
+                        if (datCampos != null)
+                        {
+                            foreach (DataColumn dc in datCampos.Columns)
+                            {
+                                propiedades += string.Format("Private {0}_{1};{2}", dc.DataType.Name, dc.ColumnName.ToLower(),Environment.NewLine);
+                                metodos += string.Format("public bool {0} {",dc.ColumnName);
+
+        
+        //    get{
+        //        return m_despachado;
+        //    }
+        //    set{
+        //        m_despachado = value;
+        //    }
+        //}
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static DataTable cargarEsquemaBD()
+        {
+            DataTable tabla;
+            OleDbConnection conexion = new OleDbConnection(ConfigurationManager.ConnectionStrings["BaseDatos"].ConnectionString);
+
+            try
+            {
+                conexion.Open();
+                tabla = conexion.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                return tabla;
+            }
+            catch (Exception ex)
+            { }
+            finally
+            {
+                conexion.Close();
+            }
+
+            return null;
+        }
+
+        public static DataTable cargarEsquemaTabla(string NombreTabla)
+        {
+            DataTable tabla = new DataTable();
+            OleDbConnection conexion = new OleDbConnection(ConfigurationManager.ConnectionStrings["BaseDatos"].ConnectionString);
+
+            try
+            {
+                if (NombreTabla != null)
+                {
+                    OleDbCommand comando = new OleDbCommand("Select * from " + NombreTabla + " Where 0=1", conexion);
+                    OleDbDataAdapter adaptador = new OleDbDataAdapter(comando);
+                    tabla = new DataTable();
+                    adaptador.Fill(tabla);
+                    adaptador.FillSchema(tabla, SchemaType.Source);
+                }
+                return tabla;
+            }
+            catch (Exception ex)
+            { }
+            finally
+            {
+                conexion.Close();
+            }
+
+            return null;
+        }
+    }
+}
